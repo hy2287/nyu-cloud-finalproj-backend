951865fba34bd5eaab3c128530e2a805f90bcce2
hy2287 <you@example.com>
2022-04-27 14:44:39 -0400

    Update lambda endpoints handler

--- lambda_func/lf2/lf2.py
+++ lambda_func/lf2/lf2.py
@@ -32,14 +32,95 @@ def respond(err, res=None):
         },
     }
 
-def getTop3Players(hours, number=3, sentiment=None):
-    queryString = 'SELECT "player_full_name", COUNT(*) FROM "AwsDataCatalog"."test"."alltweets" WHERE "created_at" > NOW() - INTERVAL '
-    queryString += "'"+str(hours)+"'" + ' HOUR'
-        
-    if sentiment=="POSITIVE" or sentiment=="NEGATIVE":
-        queryString +=  ' AND "sentiment"=' + "'"+sentiment+"'"
+def getTopPlayersHandler(params):
+    if params is None:
+        return getTopPlayers()
+    interval = int(params['interval']) if 'interval' in params else 1
+    number = params['count'] if 'count' in params else 3
+    sentiment = params['sentiment'] if 'sentiment' in params else None
+    return getTopPlayers(interval, number, sentiment)
     
-    queryString += ' GROUP BY "player_full_name" ORDER BY COUNT(*) DESC LIMIT ' + str(number)
+def getTopPlayers(interval=1, number=3, sentiment=None):
+    queryStringTemplate = "WITH temp1 AS (SELECT \"player_full_name\", COUNT(*) as count, \
+        RANK() over (ORDER BY COUNT(*) DESC) as ranking FROM \"AwsDataCatalog\".\"test\".\"alltweets\" \
+        WHERE \"created_at\" > NOW() - INTERVAL '{}' HOUR \
+        AND \"sentiment\"={} \
+        GROUP BY \"player_full_name\" ORDER BY COUNT(*) DESC LIMIT {}), \
+        temp2 AS (SELECT \"player_full_name\", COUNT(*) as past_count, \
+        RANK() over (ORDER BY COUNT(*) DESC) as past_ranking FROM \"AwsDataCatalog\".\"test\".\"alltweets\" \
+        WHERE \"created_at\" > NOW() - INTERVAL '{}' HOUR AND \"created_at\" <= NOW() - INTERVAL '{}' HOUR \
+        AND \"sentiment\"={} \
+        GROUP BY \"player_full_name\" ORDER BY COUNT(*) DESC) \
+        SELECT temp1.*, past_count, past_ranking FROM temp1 LEFT OUTER JOIN temp2 ON temp1.\"player_full_name\"=temp2.\"player_full_name\";"
+    if sentiment is None:
+        sentiment = '\"sentiment\"'
+    else:
+        sentiment = "'"+sentiment+"'"
+    queryString = queryStringTemplate.format(interval,sentiment,number,2*interval,interval,sentiment)
+    s3location, athenaResult = queryAthena(queryString)
+    return athenaResult
+
+def getPlayerHandler(params):
+    if params is None:
+        return None
+    fullname = params['fullname']
+    start = int(params['starttime']) if 'starttime' in params else 10000
+    end = int(params['endtime']) if 'endtime' in params else 0
+    return getPlayer(fullname,start,end)
+
+def getPlayer(fullname,start,end):
+    result={}
+    queryStringTemplate = "SELECT \"player_full_name\", count, ranking \
+        FROM (SELECT \"player_full_name\", COUNT(*) as count, \
+        RANK() over (ORDER BY COUNT(*) DESC) as ranking FROM \"AwsDataCatalog\".\"test\".\"alltweets\" \
+        WHERE \"created_at\" > NOW() - INTERVAL '{}' HOUR AND \"created_at\" < NOW() - INTERVAL '{}' HOUR \
+        GROUP BY \"player_full_name\") \
+        WHERE \"player_full_name\"='{}';"
+    queryString = queryStringTemplate.format(start,end,fullname)
+    s3location, athenaResult = queryAthena(queryString)
+    result['ALL'] = athenaResult
+    
+    for sentiment in ['POSITIVE','NEGATIVE','NEUTRAL']:
+        queryStringTemplate = "SELECT \"player_full_name\", count, ranking \
+            FROM (SELECT \"player_full_name\", COUNT(*) as count, \
+            RANK() over (ORDER BY COUNT(*) DESC) as ranking FROM \"AwsDataCatalog\".\"test\".\"alltweets\" \
+            WHERE \"created_at\" > NOW() - INTERVAL '{}' HOUR AND \"created_at\" < NOW() - INTERVAL '{}' HOUR \
+            AND \"sentiment\"='{}' \
+            GROUP BY \"player_full_name\") \
+            WHERE \"player_full_name\"='{}';"
+        queryString = queryStringTemplate.format(start,end,sentiment,fullname)
+        s3location, athenaResult = queryAthena(queryString)
+        result[sentiment] = athenaResult
+    return result
+
+def getTweetsHandler(params):
+    if params is None:
+        return None
+    fullname = params['fullname']
+    start = int(params['start']) if 'start' in params else 10000
+    end = int(params['end']) if 'end' in params else 0
+    number = params['count'] if 'count' in params else 3
+    return getTweets(fullname,start,end,number)
+
+def getTweets(fullname,start,end,number):
+    queryStringTemplate = "(SELECT * FROM \"AwsDataCatalog\".\"test\".\"alltweets\" \
+        WHERE \"player_full_name\"='{}' \
+        AND \"created_at\" > NOW() - INTERVAL '{}' HOUR \
+        AND \"created_at\" < NOW() - INTERVAL '{}' HOUR \
+        AND \"sentiment\"='POSITIVE' ORDER BY \"created_at\" DESC LIMIT {})\n\
+        UNION\n\
+        (SELECT * FROM \"AwsDataCatalog\".\"test\".\"alltweets\" \
+        WHERE \"player_full_name\"='{}' \
+        AND \"created_at\" > NOW() - INTERVAL '{}' HOUR \
+        AND \"created_at\" < NOW() - INTERVAL '{}' HOUR \
+        AND \"sentiment\"='NEGATIVE' ORDER BY \"created_at\" DESC LIMIT {})\n\
+        UNION\n\
+        (SELECT * FROM \"AwsDataCatalog\".\"test\".\"alltweets\" \
+        WHERE \"player_full_name\"='{}' \
+        AND \"created_at\" > NOW() - INTERVAL '{}' HOUR \
+        AND \"created_at\" < NOW() - INTERVAL '{}' HOUR \
+        AND \"sentiment\"='NEUTRAL' ORDER BY \"created_at\" DESC LIMIT {});"
+    queryString = queryStringTemplate.format(fullname,start,end,number,fullname,start,end,number,fullname,start,end,number)
     s3location, athenaResult = queryAthena(queryString)
     return athenaResult
     
@@ -77,37 +158,29 @@ def queryAthena(queryString):
                 return location, None
         else:
             time.sleep(5)
+
+def router(path,httpMethod,params):
+    if path=="/topplayers" and httpMethod=="GET":
+        return respond(None, getTopPlayersHandler(params))
+    elif path=="/player" and httpMethod=="GET":
+        return respond(None, getPlayerHandler(params))
+    elif path=="/tweets" and httpMethod=="GET":
+        return respond(None, getTweetsHandler(params))
+    elif path=="/charts" and httpMethod=="GET":
+        return respond(None, getQuickSightUrl())
+    else:
+        # quicksightRes = getQuickSightUrl()
+        # athenaRes = getTopPlayers(10000, 0, 10, "NEGATIVE")
+        
+        # respondBody = {
+        #     'athenaQueryRes': athenaRes,
+        #     'quicksightRes': quicksightRes
+        # }
+        respondBody = "Invalid http method/endpoint!"
+        return respond(None, respondBody)
+            
     
 def lambda_handler(event, context):
-    '''Demonstrates a simple HTTP endpoint using API Gateway. You have full
-    access to the request and response payload, including headers and
-    status code.
-
-    To scan a DynamoDB table, make a GET request with the TableName as a
-    query string parameter. To put, update, or delete an item, make a POST,
-    PUT, or DELETE request respectively, passing in the payload to the
-    DynamoDB API as a JSON body.
-    '''
     print(event)
-    quicksightRes = getQuickSightUrl()
-    athenaRes = getTop3Players(2400, 10, "NEGATIVE")
-    
-    respondBody = {
-        'athenaQueryRes': athenaRes,
-        'quicksightRes': quicksightRes
-    }
-    return respond(None, respondBody)
-
-    # operations = {
-    #     'DELETE': lambda dynamo, x: dynamo.delete_item(**x),
-    #     'GET': lambda dynamo, x: b,
-    #     'POST': lambda dynamo, x: dynamo.put_item(**x),
-    #     'PUT': lambda dynamo, x: dynamo.update_item(**x),
-    # }
-
-    # operation = event['httpMethod']
-    # if operation in operations:
-    #     payload = event['queryStringParameters'] if operation == 'GET' else json.loads(event['body'])
-    #     return respond(None, operations[operation](dynamo, payload))
-    # else:
-    #     return respond(ValueError('Unsupported method "{}"'.format(operation)))
+    params = event['queryStringParameters'] if 'queryStringParameters' in event else None
+    return router(event['path'],event['httpMethod'],params)
